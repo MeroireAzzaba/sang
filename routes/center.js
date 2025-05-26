1af90ab12ee9edb20f53072763d6a37bff2b5ecd
MeroireAzzaba <belhaciniines200@gmail.com>
2025-05-26 18:35:48 +0100

    second commit

--- routes/center.js
+++ routes/center.js
@@ -2,6 +2,13 @@ const { PrismaClient } = require('@prisma/client');const express = require('expr
 const router = express.Router()
 
 const prisma = new PrismaClient()
+router.get('/edit',
+  async (req,res)=>{
+    console.log(req.params.id)
+    res.send(await prisma.center.findUnique({
+      where: { id: 'd569ddc8-4fd6-4def-a377-c84a5af2055c' }
+    }))
+})
 router.route('/')
   .get(async (req, res) => {     res.send(await prisma.center.findMany())  })
   .post(async (req, res) => {    await prisma.center.create({ data:req.body })
